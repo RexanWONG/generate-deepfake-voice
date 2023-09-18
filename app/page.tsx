fc728b99f654a405e6772e87d17bc83f3c8f0419
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-09-18 14:40:27 +0800

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import WordHeader from '@/components/WordHeader'
+
+const page = () => {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <div className='mt-16'>
+        <WordHeader />
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ components/WordHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WordHeader = () => {
+  return (
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Your voice {'<>'} deepfake voice
+    </h1>
+  )
+}
+
+export default WordHeader
